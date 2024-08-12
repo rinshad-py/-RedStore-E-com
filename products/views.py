58a938942f3eaed9449fe08be5031ab955c2de4d
rinshad-py <rinjurinshad470@gmail.com>
2024-08-13 01:35:34 +0530

    added customs templates, registered

--- products/views.py
+++ products/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from . models import Product
 # Create your views here.
-def about(request):
-    return render(request, 'about.html')
-
-def contact(request):
-    return render(request, 'contact.html')
-
 
 def index(request):
     return render(request, 'index.html')
 
 def list_products(request):
     #returns product list page
-    
-    return render(request, 'products.html')
+    product_list = Product.objects.all()
+    context = {'products': product_list}
+
+    return render(request, 'products.html', context)
 
 def detail_product(request):
     return render(request, 'product_detail.html')
 
+
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
