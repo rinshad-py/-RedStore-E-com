2b1a6bc490f9ea4a82d0bcec26f1c4211ee491fa
rinshad-py <rinjurinshad470@gmail.com>
2024-08-12 13:55:39 +0530

    product, about us, contact pages added

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
+    path('products/', views.list_products, name='list_product'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+
 ]
--- products/views.py
+++ products/views.py
@@ -4,3 +4,17 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def list_products(request):
+    #returns product list page
+    
+    return render(request, 'products.html')
+
+def detail_product(request):
+    return render(request, 'product_detail.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
