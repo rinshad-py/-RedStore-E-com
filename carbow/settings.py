c82ace93fd3e4922d27e903781f515c2b88a816a
rinshad-py <rinjurinshad470@gmail.com>
2024-08-19 23:33:24 +0530

    collected static files

--- carbow/settings.py
+++ carbow/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',     #deployment vercel
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
